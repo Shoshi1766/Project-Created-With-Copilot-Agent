220e6531be6044786c1d8a32a66cb715e792c589
Shoshi- Leah Pories <s556740969@gmail.com>
2025-06-09 12:54:57 +0300

    Update App.jsx to see img logo

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           </div>
         </div>
         <img
-          src="/public/softer developer (3).png"
+          src="/softer developer (3).png"
           alt="Logo"
           style={{ width: '80px', height: '80px', borderRadius: '50%', marginRight: '10px' }}
         />
